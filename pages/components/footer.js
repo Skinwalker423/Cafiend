a4483f7ef62504f5f8258a0fcf3b646b7957c683
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-12 17:31:23 -0700

    Cafiend

---
+++ pages/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '../../styles/Home.module.css';
+import Image from 'next/image';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <div className={styles.linkIcon}>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <span>Brought to you by: Illuminati{' '} </span>
+          </a>
+          <br/>
+          <Image src={'/favicon.ico'} width={50} height={50} />
+        </div>
+      </footer>
+  )
+}
+
+export default Footer;
---
+++ pages/hello.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Hello.module.css'
+
+const Hello = () => {
+    return(
+        <div className={styles.title}>Hello</div>
+    )
+}
+
+export default Hello;
