77723f48beba243a49af26345f3b3d63ef0bc9b7
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-14 23:42:55 -0700

    _document page and font family

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, {Html, Main, NextScript, Head} from "next/document";
+
+class MyDocument extends Document{
+
+render(){
+    return (
+        <Html>
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;600;700&display=swap" rel="stylesheet"></link>
+            </Head>
+            <body>
+                <Main>
+
+                </Main>
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+}
+
+export default MyDocument;
+
