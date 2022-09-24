fa78612ec8d711f1c0efdaad2f7892c581077e65
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-23 19:23:52 -0700

    ilovedogs exercise

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const dataBody = req.query;
+  console.log("this is a test", dataBody);
+  res.status(200).json({ name: 'John Doe'})
 }
