8462e30d518feb2dac5586d55dc5862492ed0fb5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-26 00:37:49 -0700

    create record api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -12,38 +12,50 @@ const createCoffeeStore = async(req, res) => {
 
     if(method === 'POST'){
         const findStore = await table.select({
-            filterByFormula: `id="542153fa498e32acecefc77a"`
+            filterByFormula: `id="423"`
         }).firstPage();
         
-        if(findStore){
+        if(findStore.length > 0){
             const fields = findStore.map((field) => {
-                if(field.fields){
                     return field.fields;
-                }
             });
             
-            res.json({id:fields[0].id, address: fields[0].address, neighborhood: fields[0].neighborhood, imageUrl: fields[0].imageUrl, votes: fields[0].votes });
+            res.json(fields[0]);
         
         } else {
-            res.json({message: 'not found. creating store'});
-            await table.create([
+            try{
+            const createdRecord = await table.create([
                 {
                     "fields": {
-                        "id": query.id,
-                        "name": query.name,
-                        "address": query.address,
-                        "neighborhood": query.neighborhood,
-                        "votes": query.votes,
-                        "imageUrl": query.imageUrl,
+                        id: query.id,
+                        name: query.name,
+                        address: query.address,
+                        neighborhood: query.neighborhood,
+                        votes: parseInt(query.votes),
+                        imageUrl: query.imageUrl,
                     }
                 }
             ]
             )
+
+            if(createdRecord.length > 0){
+            const newRecord = createdRecord.map((field) => {
+                    return field.fields;
+            });
+
+            res.json(newRecord[0]);
+            
+            };
+
+
+            }catch(e){
+                res.json({error: 'problem creating new record', e})
+            }
         }
     }
     }catch(err){
         console.log(err);
-        res.status(500).res.json({errorMsg: err})
+        res.status(500).json({errorMsg: err})
     }
 
     res.json({message: method})
