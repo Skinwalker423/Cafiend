60a6f710a636d746f3642d7099dd353d8beebd33
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-27 21:11:21 -0700

    type guards

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 const Airtable = require('airtable');
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 
@@ -13,6 +12,9 @@ const createCoffeeStore = async(req, res) => {
     try{
 
     if(method === 'POST'){
+        if(!id){
+            return res.status(400).json({message: 'no store id was found'})
+        }
         const findStore = await table.select({
             filterByFormula: `id=${id}`
         }).firstPage();
@@ -25,46 +27,46 @@ const createCoffeeStore = async(req, res) => {
             res.json(fields[0]);
         
         } else {
-            if(id && name){
-                try{
-                const createdRecord = await table.create([
-                    {
-                        "fields": {
-                            id,
-                            name,
-                            address,
-                            neighborhood,
-                            votes,
-                            imageUrl,
-                        }
+            if(!name){
+                return res.status(400).json({message: 'name is missing'})
+            }
+
+            try{
+            const createdRecord = await table.create([
+                {
+                    "fields": {
+                        id,
+                        name,
+                        address,
+                        neighborhood,
+                        votes,
+                        imageUrl,
                     }
-                ]
-                )
-
-                if(createdRecord.length > 0){
-                const newRecord = createdRecord.map((field) => {
-                        return field.fields;
-                });
-
-                res.json(newRecord[0]);
-                
-                };
-
-
-                }catch(e){
-                    res.json({error: 'problem creating new record', e})
                 }
-            } else {
-                res.status(400).json({message: 'id and name are missing'})
+            ]
+            )
+
+            if(createdRecord.length > 0){
+            const newRecord = createdRecord.map((record) => {
+                    return record.fields;
+            });
+
+            return res.json(newRecord[0]);
+            
+            };
+
+
+            }catch(e){
+                return res.json({error: 'problem creating new record', e})
             }
         }
     }
     }catch(err){
         console.log(err);
-        res.status(500).json({errorMsg: err})
+        return res.status(500).json({errorMsg: "Error finding a store", err})
     }
 
-    res.json({message: method})
+    return res.json({message: "couldn't find your request"})
 
 }
 
