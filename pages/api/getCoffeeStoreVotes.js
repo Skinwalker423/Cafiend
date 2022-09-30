f2f197cda14541938bb282fa13067a7b71ce46c6
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-29 22:41:44 -0700

    getCoffeeStoreVotes

--- pages/api/getCoffeeStoreVotes.js
+++ pages/api/getCoffeeStoreVotes.js
@@ -17,14 +17,30 @@ const getCoffeeStoreVotes = async(req, res) => {
             filterByFormula: `id="${id}"`
         }).firstPage();
 
-        console.log('found the id and generating votes from airtable')
+        console.log('found the id and generating votes from airtable');
         
         if(findStore.length > 0){
             const fields = findStore.map((record) => {
                     return record.fields;
             });
+
+            const votes = fields[0].votes + 1;
+            const recId = fields[0].RecordID;
+
+            console.log('mapping for recId:', recId);
+
+            const updatedStore = await table.update(recId, {
+                "votes": votes,
             
-            res.json(fields[0].votes);
+            })
+
+            console.log('updating record:', updatedStore.fields.votes);
+
+
+            const updatedVotes = updatedStore.fields.votes;
+
+            
+            res.json(updatedVotes);
         
         } else {
             return res.status(400).json({message: 'no record found'})
